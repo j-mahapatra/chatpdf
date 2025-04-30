75b2c80acd89304717f23458f0bd939cf737945c
j-mahapatra <jagannathm.official@gmail.com>
2025-04-30 23:53:06 +0530

    feat: pricing modal

--- src/components/ManageSubscription.tsx
+++ src/components/ManageSubscription.tsx
@@ -4,10 +4,17 @@ import { useUser } from '@clerk/nextjs';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Loader, Rocket, Settings2 } from 'lucide-react';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from '@/components/ui/dialog';
 
 type ManageSubscriptionProps = {
   className?: string;
@@ -17,6 +24,7 @@ export default function ManageSubscription({
   className,
 }: ManageSubscriptionProps) {
   const { user } = useUser();
+  const [isCardVisible, setIsCardVisible] = useState<boolean>(false);
 
   const { data: isPlusUser, isLoading: isSubscriptionCheckLoading } = useQuery({
     queryKey: ['plus', user?.id],
@@ -45,25 +53,88 @@ export default function ManageSubscription({
     refetchOnMount: false,
   });
 
+  const plans = useMemo(() => {
+    return [
+      {
+        name: 'Free',
+        price: '₹0',
+        features: ['3 PDFs', 'Basic features', 'Limited support'],
+        cta: "I'm good",
+        ctaClick: () => setIsCardVisible(false),
+      },
+      {
+        name: 'Plus',
+        price: '₹500',
+        features: ['Unlimited PDFs', 'Extended features', 'Dedicated support'],
+        cta: 'Upgrade',
+        ctaClick: () => refetch(),
+      },
+    ];
+  }, [refetch]);
+
   return (
-    <Button
-      onClick={() => refetch()}
-      className={cn('flex justify-center items-center', className)}
-      disabled={isLoading}
-    >
-      {(isLoading || isSubscriptionCheckLoading) && (
-        <Loader className='w-6 h-6 animate-spin' />
-      )}
-      {isLoading || isSubscriptionCheckLoading ? null : isPlusUser ? (
-        <Settings2 className='w-6 h-6 mr-2' />
-      ) : (
-        <Rocket className='w-6 h-6 mr-2' />
-      )}
-      {isLoading || isSubscriptionCheckLoading
-        ? null
-        : isPlusUser
-          ? 'Manage Subscription'
-          : 'Upgrade to PLUS'}
-    </Button>
+    <>
+      <Button
+        onClick={() => {
+          if (isPlusUser) {
+            refetch();
+          } else {
+            setIsCardVisible(true);
+          }
+        }}
+        className={cn('flex justify-center items-center', className)}
+        disabled={isLoading}
+      >
+        {(isLoading || isSubscriptionCheckLoading) && (
+          <Loader className='w-6 h-6 animate-spin' />
+        )}
+        {isLoading || isSubscriptionCheckLoading ? null : isPlusUser ? (
+          <Settings2 className='w-6 h-6 mr-2' />
+        ) : (
+          <Rocket className='w-6 h-6 mr-2' />
+        )}
+        {isLoading || isSubscriptionCheckLoading
+          ? null
+          : isPlusUser
+            ? 'Manage Subscription'
+            : 'Upgrade to PLUS'}
+      </Button>
+      <Dialog
+        open={isCardVisible && !isPlusUser}
+        onOpenChange={() => setIsCardVisible((prev) => !prev)}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <div className='grid gap-6 grid-cols-2 p-6'>
+                {plans.map((plan) => (
+                  <Card
+                    key={plan.name}
+                    className='flex flex-col justify-between items-start rounded-2xl shadow-md'
+                  >
+                    <CardHeader>
+                      <CardTitle className='text-xl'>{plan.name}</CardTitle>
+                      <p className='text-3xl font-bold'>{plan.price}</p>
+                    </CardHeader>
+                    <CardContent>
+                      <ul className='flex flex-col items-start mb-6 space-y-2'>
+                        {plan.features.map((feature) => (
+                          <li key={feature} className='text-muted-foreground'>
+                            • {feature}
+                          </li>
+                        ))}
+                      </ul>
+                      <Button className='w-full' onClick={plan.ctaClick}>
+                        {plan.cta}
+                      </Button>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
